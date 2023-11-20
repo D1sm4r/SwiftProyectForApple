520be4fd1a63f5422b4deb9a9265954e2aa70e34
D1sm4r <Dismar719@gmail.com>
2023-11-20 20:29:53 -0300

    Initial Commit

---
+++ Prueba/PruebaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PruebaApp.swift
+//  Prueba
+//
+//  Created by Dismar Quiroz on 20-11-23.
+//
+
+import SwiftUI
+
+@main
+struct PruebaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
