c7c9debc25562cd15d6a0187a0f41d66e30c9860
D1sm4r <Dismar719@gmail.com>
2024-01-11 16:25:38 -0300

    Prueba 2

--- Prueba/ContentView.swift
+++ Prueba/ContentView.swift
@@ -5,19 +5,18 @@
 //  Created by Dismar Quiroz on 20-11-23.
 //
 
+import MapKit
 import SwiftUI
+
+extension CLLocationCoordinate2D{
+    static let parking = CLLocationCoordinate2D(latitude: -36.8294411, longitude: -73.0594820)
+}
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Map{
+            Marker("Parking", coordinate: .parking)
         }
-        .padding()
-        
-        
     }
 }
 
